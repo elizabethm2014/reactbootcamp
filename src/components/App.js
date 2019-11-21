4f8eb04483c7e57c1186a56ca41178409403dc78
Elizabeth Freeman <efreeman@lenovo.com>
2019-11-21 13:34:00 -0500

    FileUpload

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <FileUpload/>
                 <img src={profile} alt="profile" className='profile' />
                 <h1>Hello!</h1>
                 <p>My name is Elizabeth.</p>
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 
-class QuotePage extends Component {
-    onFileChangeHandler = (e) => {
-         e.preventDefault();
-         this.setState({
-            selectedFile: e.target.files[0]
-        });
+class FileUpload extends Component {
+    constructor() {
+        super();
+        this.state = {selectedFile: ''}
+    }
 
-        console.log("on file change handler");
+    onFileChangeHandler = (event) => {
+        event.preventDefault();
+        const isValidSize = this.validateSize(event);
+        if (isValidSize) {
+            console.log("add selected file");
+            this.setState({
+                selectedFile: event.target.files[0]
+            });
+        }
+
     };
 
+    validateSize = (event) => {
+        let maxSize = 100000000; // 100 MB
+        let err = '';
+        let file = event.target.files[0];
+        console.log('file', file.name, 'file size =',file.size);
+        if (file.size > maxSize) {
+            err = file.name + ' exceeds the size limit of 100MB\n';
+            alert(err);
+            return false
+        }
+        return true
+    };
 
     uploadFile = () => {
-        // const formData = new FormData();
-        // formData.append('file', this.state.selectedFile);
+        let  file = this.state.selectedFile;
+        const formData = new FormData();
+        formData.append('file', file);
+        // TODO - implement upload of file
         // fetch('http://localhost:8080/upload', {
         //     method: 'post',
         //     body: formData
@@ -23,18 +45,21 @@ class QuotePage extends Component {
         //         alert("File uploaded successfully.")
         //     }
         // });
-        console.log("Upload file", this.state.selectedFile.name);
+        console.log("Upload file", file.name);
     }
 
     render() {
-        return(
-            <div>
-                <div>
-                    <div>
-                        <div className="form-group">
-                            <label>Upload Your File</label>
-                            <input type="file" className="form-control" name="file" onChange={this.onFileChangeHandler}/>
-                            <input type="button" name="upload" value="Upload" onClick={this.uploadFile}/>
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="form-group files">
+                            <label>Upload File </label>
+                            <input type="file" className="form-control" name="file"
+                                   onChange={this.onFileChangeHandler}/>
+                        </div>
+                        <div className="col-md-6 pull-right">
+                            <button width="100%" className="btn btn-info" onClick={this.uploadFile}>Upload</button>
                         </div>
                     </div>
                 </div>
@@ -43,4 +68,4 @@ class QuotePage extends Component {
     }
 }
 
-export default QuotePage
+export default FileUpload
